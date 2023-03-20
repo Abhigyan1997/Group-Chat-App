568ef7313ddd2bb28242262b2133f3da2c1d6b54
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-03-20 22:51:14 +0530

    SignUp Backend

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs=require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet=require('helmet');
+const compression=require('compression');
+const morgan=require('morgan');
+const app = express();
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+const sequelize = require('./util/database');
+
+//const User=require('./models/user');
+
+
+const accessLogStream=fs.createWriteStream(
+    path.join(__dirname,'access.log'),
+    {flags:'a'}
+    );
+
+var cors = require('cors');
+app.use(cors());
+
+const userRoutes = require('./routes/user');
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined',{stream:accessLogStream}));
+
+app.use(bodyParser.json({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use('/user',userRoutes);
+
+
+sequelize 
+ .sync({force:true})
+ .then(result => {
+    app.listen(5000);
+ })
+ .catch(err => {
+    console.log(err);
+ }) 
---
+++ controller/user.js
@@ -0,0 +1,22 @@
+const User=require('../models/user');
+  const bcrypt=require('bcrypt');
+  const jwt=require('jsonwebtoken');
+
+  exports.signup= async (req , res)=>{
+    try{
+    const{name, email, phone, password}=req.body;
+    if(name===undefined||name.length===0||password==null||password.length===0||email==null||email.lenght===0||phone===undefined||phone.length===0){
+        return res.status(400).json({err:'Bad Paramete,Something is missing'})
+
+    }
+    const saltrounds=10;
+    bcrypt.hash ( password,saltrounds,async(err , hash)=>{
+      console.log(err);
+      await User.create({name,email,password:hash})
+      res.status(201).json({message:"Succesfully created new user"})
+    })
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+  }
