09d2728c01545133cbc6d2f70c6dc0591b8f8e42
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-03-27 10:31:53 +0530

    Real Time Chat App

--- Chat Frontend/script.js
+++ Chat Frontend/script.js
@@ -17,20 +17,55 @@ function sendMessage(event) {
       });
   }
 
-  document.addEventListener("DOMContentLoaded", async () => {
-    try {
-        const token = localStorage.getItem("token")
-        const response = await axios.get("http://localhost:5000/user/getmessage",{ headers: { Authorization: token } })
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get("http://localhost:5000/user/getmessage", {
+      headers: { Authorization: token },
+    });
 
-        if (response.status==201) {
-            for(let i=0; i<response.data.message.length;i++){
-                showOnChatBox(response.data.message[i].username, response.data.message[i].message)
-            }
-        }
-    } catch (err) {
-        console.log(err)
+    if (response.status == 201) {
+      // Clear the chat box
+      document.getElementById("chat-messages").innerHTML = "";
+
+      // Display the messages from localStorage
+      const messages = JSON.parse(localStorage.getItem("messages")) || [];
+      messages.forEach((msg) => showOnChatBox(msg.username, msg.message));
+
+      // Display the messages from the server
+      for (let i = 0; i < response.data.message.length; i++) {
+        showOnChatBox(
+          response.data.message[i].username,
+          response.data.message[i].message
+        );
+      }
     }
-})
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Poll the server every 1 seconds
+  setInterval(async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get("http://localhost:5000/user/getmessage", {
+        headers: { Authorization: token },
+      });
+
+      if (response.status == 201) {
+        // Display the new messages
+        for (let i = 0; i < response.data.message.length; i++) {
+          showOnChatBox(
+            response.data.message[i].username,
+            response.data.message[i].message
+          );
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, 1000);
+});
 
 function showOnChatBox(username, message) {
     const parentnode = document.getElementById("chat-messages")
